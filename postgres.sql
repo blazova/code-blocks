e11d4f5316f6973f592430c823563b2671f93fdc
lucia blazova <39126832+blazova@users.noreply.github.com>
2021-07-08 09:50:29 +0200

    Update postgres.sql

--- postgres.sql
+++ postgres.sql
@@ -2,6 +2,7 @@ heroku pg:psql -a name_of_database
 
 \dt and \dv display tables/views in the database
 \dt lists tables for public schema. To show tables of all schemas use \dt *.* and for a particular schema use \dt schema_name.*.
+\dn list all schemas
 
 CREATE [OR REPLACE] VIEW name_of_view AS (<SQL query>);
 
@@ -9,6 +10,7 @@ CREATE [OR REPLACE] VIEW name_of_view AS (<SQL query>);
 DROP VIEW name_of_view [CASCADE]; --deletes the view, if CASCADE is written, all other objects depending on the view are deleted as well.
 
 ALTER TABLE table_name DROP COLUMN column_name; -- to drop certain columns in a table, no usage needs to be re-granted after altering the tables.
+ALTER TABLE customers ADD COLUMN phone VARCHAR;
 
 GRANT SELECT ON view_flow_sessions_dog TO kianskolqhmzag;
 

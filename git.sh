c499053fdccdc8e409f760d9eff3caae08d61396
lucia blazova <39126832+blazova@users.noreply.github.com>
2021-09-20 11:21:50 +0200

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,3 +1,18 @@
 #create a new branch and switch directly https://www.banterly.net/2021/07/31/new-in-git-switch-and-restore/
 git checkout -b new_branch
 
+#add tag
+git tag -a v0.3.3 -m "version v0.3.3"
+git push origin main --tags          
+
+#delete local tags
+$ git tag -d <tag_name>
+
+#delete remote tags
+$ git push --delete origin tagname
+
+#delete local branch
+$ git branch -d <local-branch>
+
+#delete local branches that have been merged into main
+$ git fetch --prune 

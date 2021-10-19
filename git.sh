7b1d0a2822ba6b560f660ceffef8c85651e0ba18
lucia blazova <39126832+blazova@users.noreply.github.com>
2021-10-19 14:13:08 +0200

    Update git.sh

--- git.sh
+++ git.sh
@@ -5,6 +5,11 @@ git checkout -b new_branch
 git tag -a v0.3.3 -m "version v0.3.3"
 git push origin main --tags          
 
+#add temp tag v-e2- on a specific branch feature-accomodate-new-secrets and push
+#the branch with tags will have different versions, and after merging to main will bump the main's version
+git tag -a v-e2-0.0.5 -m "version v-e2-0.0.5"       
+git push origin feature-accomodate-new-secrets --tags
+
 #delete local tags
 $ git tag -d <tag_name>
 # then push
